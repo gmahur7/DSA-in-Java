cc03eb575d8b737378559909e46197dcf7bc47db
gmahur7 <gm7017308@gmail.com>
2026-04-28 08:42:33 +0530

    add: Q24 in array/questions

---
+++ Array/Questions/Q24_Count_SubArray_With_Sum_K.java
@@ -0,0 +1,26 @@
+public class Q24_Count_SubArray_With_Sum_K {
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 1, 1, 1, 5, 6, 4, 7, 2, 3, 1, 0, 5 };
+        int target = 6;
+        int count = getCount(nums, target);
+        System.out.println("SubArrays with given target Count: " + count);
+    }
+
+    static int getCount(int nums[], int target) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                if (sum == target) {
+                    // System.out.println("i: " + i + " & j: " + j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
